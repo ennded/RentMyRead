36bcc0bd97ef1b01794a4b3aad41328196cfb9e3
sangam gautam <97830458+ennded@users.noreply.github.com>
2025-05-11 23:21:11 +0530

    set up server , mongodb and middleware

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//middleware
+app.use(helmet()); //security headers
+app.use(cors()); // cross orgin resource sharing
+app.use(express.json()); // body parser
+
+connectDB();
+
+//test route
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Server is Running",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
