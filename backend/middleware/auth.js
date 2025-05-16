9110b38fc86e2cf46bb96ddee363008fea5c00b5
sangam gautam <97830458+ennded@users.noreply.github.com>
2025-05-17 00:41:34 +0530

    auth.js completed

---
+++ backend/middleware/auth.js
@@ -0,0 +1,29 @@
+const { verifyToken } = require("../config/jwt");
+const User = require("../models/User");
+
+//atthentication middleware
+
+const protect = async (req, res, next) => {
+  let token;
+  if (req.headers.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, token missing" });
+  }
+
+  try {
+    const decode = verifyToken(token);
+    const user = await User.findById(decode.id);
+
+    if (!user) return res.status(401).json({ message: "User not found" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = { protect };
