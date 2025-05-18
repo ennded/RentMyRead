f64d3570618a6064efbcd8a01d73f34cf9087574
sangam gautam <97830458+ennded@users.noreply.github.com>
2025-05-18 22:10:57 +0530

    passport added in server.js

---
+++ backend/config/passport.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GitHubStrategy = require("passport-github2").Strategy;
+const User = require("../models/User");
+const { generateToken } = require("./jwt");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(err);
+  }
+});
+
+// google strategy
+passport.new(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      await handleSocialLogin(profile, "google", done);
+    }
+  )
+);
+
+//
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const helmet = require("helmet");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const passport = require("passport");
+require("./config/passport");
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(helmet()); //security headers
 app.use(cors()); // cross orgin resource sharing
 app.use(express.json()); // body parser
 app.use("/api/auth", authRoutes);
+app.use(passport.initialize());
 
 connectDB();
 
