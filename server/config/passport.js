fe103a266e73bac2f46f289e74deb4336bc8f677
sangam gautam <97830458+ennded@users.noreply.github.com>
2025-05-25 21:21:17 +0530

    updated server.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,21 +5,23 @@ const GitHubStrategy = require("passport-github2").Strategy;
 const User = require("../models/User");
 const { generateToken } = require("./jwt");
 
+// Serialize user
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+// Deserialize user
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
     done(null, user);
-  } catch (error) {
+  } catch (err) {
     done(err);
   }
 });
 
-// google strategy
-passport.new(
+// Google Strategy
+passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
@@ -32,7 +34,7 @@ passport.new(
   )
 );
 
-//Facebook Strategy
+// Facebook Strategy
 passport.use(
   new FacebookStrategy(
     {
@@ -41,8 +43,8 @@ passport.use(
       callbackURL: process.env.FACEBOOK_CALLBACK_URL,
       profileFields: ["id", "emails", "name"],
     },
-    async (accessToken, refreshToken, Profiler, done) => {
-      await handleSocialLogin(Profiler, "facebook", done);
+    async (accessToken, refreshToken, profile, done) => {
+      await handleSocialLogin(profile, "facebook", done);
     }
   )
 );
@@ -51,7 +53,7 @@ passport.use(
 passport.use(
   new GitHubStrategy(
     {
-      clientID: GITHUB_CLIENT_ID,
+      clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL: process.env.GITHUB_CALLBACK_URL,
     },
@@ -64,20 +66,22 @@ passport.use(
 // Common social login handler
 const handleSocialLogin = async (profile, provider, done) => {
   try {
-    let user = await User.findOne({ email: Profiler.emails[0].value });
+    let user = await User.findOne({ email: profile.emails[0].value });
+
     if (!user) {
       user = await User.create({
         name:
-          profile.display ||
+          profile.displayName ||
           `${profile.name.givenName} ${profile.name.familyName}`,
         email: profile.emails[0].value,
-        password: `social-${provider}-{profile.id}`,
-        provider, //dummy password
+        password: `social-${provider}-${profile.id}`, // Dummy password
+        provider,
         providerId: profile.id,
       });
     }
+
     done(null, user);
-  } catch (error) {
-    done(error, null);
+  } catch (err) {
+    done(err, null);
   }
 };
